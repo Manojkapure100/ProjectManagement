e178f37347ab2501f8dbddd3e9bdc96d9b5c7fb8
Manojkapure100 <manojkapure100@gmail.com>
2023-03-21 17:51:38 +0530

    product entity removed becouse its used for sample data

--- src/main/java/com/springDataJpa/springBoot/controllers/Controller1.java
+++ src/main/java/com/springDataJpa/springBoot/controllers/Controller1.java
@@ -1,27 +1,17 @@
 package com.springDataJpa.springBoot.controllers;
-
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.springDataJpa.springBoot.entityOrModelOrDomain.Investor_project;
-import com.springDataJpa.springBoot.entityOrModelOrDomain.Product;
 import com.springDataJpa.springBoot.repositoryOrInterface.investorProjectRepo;
 import com.springDataJpa.springBoot.repositoryOrInterface.investorRepo;
-import com.springDataJpa.springBoot.repositoryOrInterface.productRepository;
 import com.springDataJpa.springBoot.repositoryOrInterface.projectRepo;
-
-
 
 @RestController
 public class Controller1 {
 
   @Autowired
-  // private productRepository productRepository;
   private investorProjectRepo investorProjectRepo;
   private investorRepo investorRepo;
   private projectRepo projectRepo;
@@ -30,25 +20,6 @@ public class Controller1 {
   public String root() {
       return "root page";
   }
-
-  // @GetMapping("/insert")
-  // public String insert(){
-  //   Product obj = new Product();
-  //   obj.setName("abc");
-  //   obj.setImageUrl("abc");
-  //   obj.setActive(true);
-  //   obj.setDescription("abbf");
-  //   obj.setPrice(null);
-  //   obj.setSku("abcd");
-
-  //   Product myproduct = productRepository.save(obj);
-  //   return display(myproduct);
-  // }
-
-  // @GetMapping("/display")
-  // public String display(Product product) {
-  //     return product.toString();
-  // }
 
   @GetMapping("/findProjectsByInvestorId")
   public String findProjectsByInvestorId(@RequestParam int id) {
