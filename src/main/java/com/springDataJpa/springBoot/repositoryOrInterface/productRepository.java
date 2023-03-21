6e4a4d2cb77c51b0ded8ab97efc60ff2a1dbbb92
Manojkapure100 <manojkapure100@gmail.com>
2023-03-21 17:59:20 +0530

    delete productrepo.java

--- src/main/java/com/springDataJpa/springBoot/repositoryOrInterface/productRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springDataJpa.springBoot.repositoryOrInterface;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springDataJpa.springBoot.entityOrModelOrDomain.Product;
-
-public interface productRepository extends JpaRepository<Product,Long>{
-
-}
